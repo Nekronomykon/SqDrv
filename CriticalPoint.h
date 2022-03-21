8dfcc3416adbc11561adb41a9a622bc6fdd5ac95
Nekronomykon <nekronomykon@gmail.com>
2022-03-21 22:18:58 +0300

    Naming

--- CriticalPoint.h
+++ CriticalPoint.h
@@ -5,9 +5,9 @@
 
 typedef std::complex<short int> CriticalPointType;
 
-static const CriticalPointType cptypeMininum(3,+3); // signature of a Cage Critical Point (CCP, local minimum)
-static const CriticalPointType cptypeSaddleR(3,+1); // signature of a Ring Critical Point (RCP)
-static const CriticalPointType cptypeSaddleP(3,-1); // signature of a Bond Critical Point (BCP)
-static const CriticalPointType cptypeMaximum(3,-3); // signature of a Atom Critical Point (ACP, local maximum)
+static const CriticalPointType cpTypeMininum(3,+3); // signature of a Cage Critical Point (CCP, local minimum)
+static const CriticalPointType cpTypeSaddleR(3,+1); // signature of a Ring Critical Point (RCP)
+static const CriticalPointType cpTypeSaddleP(3,-1); // signature of a Bond Critical Point (BCP)
+static const CriticalPointType cpTypeMaximum(3,-3); // signature of a Atom Critical Point (ACP, local maximum)
 
 #endif // !__Critical_Point_h__
