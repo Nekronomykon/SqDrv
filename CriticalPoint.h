de22605786acc736c90f850e2878240836dfbd79
Nekronomykon <nekronomykon@gmail.com>
2022-04-15 12:10:43 +0300

    Sharing the CP input functional

--- CriticalPoint.h
+++ CriticalPoint.h
@@ -7,9 +7,11 @@ typedef std::complex<short int> CriticalPointType;
 
 static const CriticalPointType::value_type FullRank(3);
 
-static const CriticalPointType cpTypeMininum(3,+3); // signature of a Cage Critical Point (CCP, local minimum)
-static const CriticalPointType cpTypeSaddleR(3,+1); // signature of a Ring Critical Point (RCP, saddle)
-static const CriticalPointType cpTypeSaddleB(3,-1); // signature of a Bond Critical Point (BCP, saddle point)
-static const CriticalPointType cpTypeMaximum(3,-3); // signature of a Atom Critical Point (ACP, local maximum)
+enum {FullRankTypeMinimum = +3, FullRankTypeSaddleR = +1, FullRankTypeSaddleB = -1, FullRankTypeMaximum = -3};
+
+static const CriticalPointType cpTypeMinimum(FullRank, FullRankTypeMinimum); // signature of a Cage Critical Point (CCP, local minimum)
+static const CriticalPointType cpTypeSaddleR(FullRank, FullRankTypeSaddleR); // signature of a Ring Critical Point (RCP, saddle)
+static const CriticalPointType cpTypeSaddleB(FullRank, FullRankTypeSaddleB); // signature of a Bond Critical Point (BCP, saddle point)
+static const CriticalPointType cpTypeMaximum(FullRank, FullRankTypeMaximum); // signature of a Atom Critical Point (ACP, local maximum)
 
 #endif // !__Critical_Point_h__
